160e0d2a3f0e3070fbda51b4ceea36b7b94b2ed6
Lindsey Jarrett <lindsey.jarrett7@gmail.com>
2022-08-31 00:12:35 -0400

    Added functions to display repo info when clicked

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const overview = document.querySelector(".overview");
 //My username
 const username = "lindzo7";
 //The unordered list that displays the repo list
-const displayRepoList = document.querySelector(".repo-list");
+const repoList = document.querySelector(".repo-list");
+//Where all the repo information appears 
+const displayRepoInfo = document.querySelector(".repos");
+//Where all the individual repo data appears
+const selectSingleRepoData = document.querySelector(".repo-data");  
 
 const getInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`); 
@@ -40,7 +44,48 @@ const repoInfo = function (repos) {
         const li = document.createElement("li");
         li.classList.add("repo");
         li.innerHTML = `<h3>${repo.name}</h3>`;
-        displayRepoList.append(li);
+        repoList.append(li);
     };
 };  
+
+repoList.addEventListener("click", function (e) {
+    if (e.target.matches("h3")) {
+        const repoName = e.target.innerText;
+        singleRepoInfo(repoName);
+    }
+});
+
+const singleRepoInfo = async function (repoName) {
+    const oneRepo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await oneRepo.json();
+    console.log(repoInfo);
+    const fetchLanguages = await fetch(repoInfo.languages_url);
+    const languageData = await fetchLanguages.json();
+    console.log(languageData);
+
+    const languages = [];
+for (const lang in languageData) {
+    languages.push(lang);
+    console.log(languages);
+    }
+ 
+displaySingleRepoData(repoInfo, languages);    
+};
+
+const displaySingleRepoData = function (repoInfo, languages) {
+    selectSingleRepoData.innerHTML = "";
+    selectSingleRepoData.classList.remove("hide");
+    displayRepoInfo.classList.add("hide");
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <h3>Name: ${repoInfo.name}</h3>
+        <p>Description: ${repoInfo.description}</p>
+        <p>Default Branch: ${repoInfo.default_branch}</p>
+        <p>Languages: ${languages.join(", ")}</p>
+        <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+    selectSingleRepoData.append(div);
+    
+};
+
+
 
